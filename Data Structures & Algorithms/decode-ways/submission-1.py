7cd6bf92650d261585fb777fe13e7c3867837d09
Farhan Ali <186649863+alifarhan7974@users.noreply.github.com>
2026-06-26 01:26:45 -0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        # Want to find dp[0]
+        dp = [0] * (n + 1) # dp[i] = number of ways to decode str from index i
+        dp[n] = 1 # Reached end; can decode whole string 
+
+
+        for i in range(n - 1, -1, -1): 
+            # Check not 0
+            if int(s[i]) != 0: 
+                dp[i] += dp[i + 1]
+
+            # s[i:i+2] needs i + 1 to exist 
+            if i + 1 < n and 10 <= int(s[i:i+2]) <= 26: 
+                dp[i] += dp[i+2]
+
+
+        return dp[0]
+        
+
+
+
+         
+
+
+
+
+     
