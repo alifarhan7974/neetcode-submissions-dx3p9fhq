59f9ed40e1610323cae0e8ea475f24397a00cbc7
Farhan Ali <186649863+alifarhan7974@users.noreply.github.com>
2026-06-26 01:33:06 -0700

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size(); 
+        vector<int> dp = vector<int>(n + 1, 0);
+        dp[n] = 1; 
+        
+        for (int i = n - 1; i >= 0; i--) { 
+            if (s[i] != '0') { 
+                dp[i] += dp[i + 1]; 
+            } 
+
+            if (i + 1 < n) { 
+                int twodigit = 10 * (s[i] - '0') + (s[i+1] - '0');
+                if (10 <= twodigit && twodigit <= 26) { 
+                    dp[i] += dp[i + 2];
+                } 
+            }
+
+        }
+
+        return dp[0]; 
+    }
+};
