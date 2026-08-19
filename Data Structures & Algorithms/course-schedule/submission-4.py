d95f94a723e3f7058b67a88dae0e162edc1fa74e
Farhan Ali <186649863+alifarhan7974@users.noreply.github.com>
2026-08-18 21:20:57 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,42 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list) # prereq > courses it can unlock
+        indegree = [0] * numCourses
+        for a, b in prerequisites: 
+            graph[b].append(a)
+            indegree[a] += 1 
+
+        state = [0] * numCourses
+        queue = deque()
+        courses = set() 
+
+        for i in range(numCourses): 
+            if indegree[i] == 0: 
+                queue.append(i)
+
+        
+        while queue: 
+            x = queue.popleft()
+            courses.add(x)
+
+            for new_course in graph[x]: 
+                indegree[new_course] -= 1 
+                if indegree[new_course] == 0: 
+                    queue.append(new_course)
+
+
+
+        return len(courses) == numCourses
+
+
+
+
+
+
+        
+
+
+
+       
