9c94c84dc89514f2ae3e960969267bc4c616c644
Farhan Ali <186649863+alifarhan7974@users.noreply.github.com>
2026-09-13 17:42:37 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,9 @@
+from collections import Counter 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_freq = Counter(s)
+        t_freq = Counter(t)
+        return s_freq == t_freq
+
+
+        
